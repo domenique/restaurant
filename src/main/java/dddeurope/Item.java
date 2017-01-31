9cb9812cd9a9c9560cc50ce15aeeb92134f82e9c
Domenique Tilleuil <Domenique.Tilleuil@gmail.com>
2017-01-31 10:17:49 +0100

    initial commit

---
+++ src/main/java/dddeurope/Item.java
@@ -0,0 +1,9 @@
+package dddeurope;
+
+class Item {
+
+  private int quantity;
+  private int price;
+  private String name;
+
+}
---
+++ src/main/java/dddeurope/Order.java
@@ -0,0 +1,18 @@
+package dddeurope;
+
+import java.util.List;
+
+class Order {
+
+  private String uuid;
+  private int tableNumber;
+  private List<Item> items;
+  private int subtotal;
+  private int taxes;
+  private int total;
+  private int cookTime;
+  private String ingredients;
+
+
+
+}
