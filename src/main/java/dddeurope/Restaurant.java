554e0ffc88336716fb78de05073ee809fdb83a9f
Domenique Tilleuil <Domenique.Tilleuil@gmail.com>
2017-01-31 12:19:40 +0100

    refactor

--- src/main/java/dddeurope/Restaurant.java
+++ src/main/java/dddeurope/Restaurant.java
@@ -7,20 +7,20 @@ class Restaurant {
   public static void main(String[] args) {
     OrderHandlerPrinter orderPrinter = new OrderHandlerPrinter();
 
-    OrderHandler assistantManager = new ThreadedHandler(new AssistantManager(orderPrinter));
+    ThreadedHandler assistantManager = new ThreadedHandler(new AssistantManager(orderPrinter));
 
-    OrderHandler gordon = new ThreadedHandler(new Cook(assistantManager, "Gordon Ramsy"));
-    OrderHandler jamie = new ThreadedHandler(new Cook(assistantManager, "Jamie Oliver"));
-    OrderHandler piet = new ThreadedHandler(new Cook(assistantManager, "Piet Huysentruyt"));
+    ThreadedHandler gordon = new ThreadedHandler(new Cook(assistantManager, "Gordon Ramsy"));
+    ThreadedHandler jamie = new ThreadedHandler(new Cook(assistantManager, "Jamie Oliver"));
+    ThreadedHandler piet = new ThreadedHandler(new Cook(assistantManager, "Piet Huysentruyt"));
 
     OrderHandler orderHandler = new RoundRobinRepeater(Arrays.asList(gordon, jamie, piet));
 
     Waiter waiter = new Waiter(orderHandler);
 
-    start(assistantManager);
-    start(gordon);
-    start(jamie);
-    start(piet);
+    assistantManager.start();
+    gordon.start();
+    jamie.start();
+    piet.start();
 
     int totalTime = 0;
     for (int i = 0; i < 10; i++) {
@@ -34,7 +34,4 @@ class Restaurant {
     System.out.println("Total cooktime : " + totalTime);
   }
 
-  private static void start(OrderHandler orderHandler) {
-    ((Startable) orderHandler).start();
-  }
 }
